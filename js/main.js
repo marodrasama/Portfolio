f346ac3914d30514f46380308f9e5c8e2d16c60c
Yogesh Sdot <Yogesh Sdot>
2016-04-05 22:04:38 +0530

    Minor Updates

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#nav-icon1').click(function(){
+        $(this).toggleClass('open');
+        $('#navbar_tab').toggleClass('open');
+        $('.banner').toggleClass('open');
+    });
+
+});
+
+
