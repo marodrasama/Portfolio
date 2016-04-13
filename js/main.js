04af2ad91ba2b8d57b4ed40483b4c8f27e4bcfa2
Yogesh Sdot <Yogesh Sdot>
2016-04-13 19:22:49 +0530

    Menu fix and Gallery removal

--- js/main.js
+++ js/main.js
@@ -7,4 +7,37 @@ $(document).ready(function(){
 
 });
 
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('#header').outerHeight();
 
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 600);
+
+function hasScrolled() {
+    var lastScrollTop = 0, delta = 5;
+    $(window).scroll(function(event){
+       var st = $(this).scrollTop();
+       
+       if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+        if (st > lastScrollTop){
+           // downscroll code
+           $("#header").css({top:'-120px'})
+           .hover(function(){$("#header").css({top: '0px'})})
+       } else {
+          // upscroll code
+          $("#header").css({top:'0px'});
+       }
+       lastScrollTop = st;
+    });
+}
